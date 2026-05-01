2d99c6a209ca6aa5b49f4fd2d40f7763145df801
José Antonio <joseantonioalmeida217@gmail.com>
2026-05-01 14:39:31 -0300

    criei o app vehicle e o model

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # meus apps
     'customers',
+    'vehicles',
 ]
 
 MIDDLEWARE = [
---
+++ vehicles/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+from customers.models import Customer
+
+class VehicleType(models.Model):
+    class Meta:
+        verbose_name = 'Tipo do Veículo'
+        verbose_name_plural = 'Tipos dos Veículos'
+    
+    def __str__(self):
+        return self.name
+    
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name="Nome",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Descrição',
+    )
+
+class Vehicles(models.Model):
+    class Meta:
+        verbose_name = 'Veículo'
+        verbose_name_plural = 'Veículos'
+
+    def __str__(self) -> str:
+        return self.license_plate
+    
+    vehicle_type = models.ForeignKey(
+        VehicleType,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='vehicles',
+        verbose_name='Tipo do Veículo',
+    )
+    license_plate = models.CharField(
+        max_length=10,
+        unique=True,
+        verbose_name='Placa',
+    )
+    brand = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Marca',
+    )
+    model = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Modelo',
+    )
+    color = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Cor',
+    )
+    owner = models.ForeignKey(
+        Customer,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='vehicles',
+        verbose_name='Proprietário'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Criado em'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Atualizado em'
+    )

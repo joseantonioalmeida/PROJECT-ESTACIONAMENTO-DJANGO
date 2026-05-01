30d8bc68c47be1b455624acdf874a8d0ea6b1528
José Antonio <joseantonioalmeida217@gmail.com>
2026-05-01 16:51:32 -0300

    exibe apenas vagas não ocupadas ao criar um registro de estacionamento

--- parking/admin.py
+++ parking/admin.py
@@ -13,3 +13,8 @@ class ParkingSpotAdmin(admin.ModelAdmin):
 class ParkingRecordAdmin(admin.ModelAdmin):
     list_display = ['vehicle', 'parking_spot', 'entry_time', 'exit_time']
     search_fields = ['vehicles__license_plate', 'parking_spot__spot_number']
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'parking_spot' and not request.resolver_match.url_name.endswith('change'): #type:ignore
+            kwargs['queryset'] = ParkingSpot.objects.filter(is_occupied=False)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)

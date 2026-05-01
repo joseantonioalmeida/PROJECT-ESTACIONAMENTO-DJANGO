496a362a67c71e0a697eca3d7c29fdfce611b6ef
José Antonio <joseantonioalmeida217@gmail.com>
2026-05-01 16:35:28 -0300

    criei o signal, que quando uma vaga for preenchida, estará ocupada=True

--- parking/apps.py
+++ parking/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ParkingConfig(AppConfig):
     name = 'parking'
     verbose_name = 'Registro'
+
+    def ready(self) -> None:
+        import parking.signals
